671edef413c9427b4ebb92e613056f1bdb56ae69
Melvin1Atieno <melvinatieno@gmail.com>
2017-11-03 11:11:40 +0300

    FIX building error by changing test directory

---
+++ tests/test_views.py
@@ -0,0 +1,37 @@
+import unittest
+from yummy.views import app
+
+
+class FlaskTestCase(unittest.TestCase):
+
+
+    def test_index_page_loads(self):
+        '''tests whether the index page loads'''
+        tester = app.test_client(self)
+        response = tester.get("/", content_type="html/text")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_registration_loads(self):
+        '''tests whether registration page loads'''
+        tester = app.test_client(self)
+        response = tester.get("/user_registration", content_type="html/text")
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page_loads(self):
+        '''tests whether the login page loads, brings status code 200'''
+        tester = app.test_client(self)
+        response = tester.get("/login", content_type="html/text")
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_loads(self):
+        '''tests whether the homepage loads '''
+        tester = app.test_client(self)
+        response = tester.get("/homepage", content_type="html/text")
+        self.assertEqual(response.status_code, 200)
+      
+
+    
+
+
+if __name__ == "__main__":
+    unittest.main()
